2e2aa8c6c8e594f030c7907906f45ee6496d23c1
Verdun-Mtl <72748729+Verdun-Mtl@users.noreply.github.com>
2021-03-02 16:43:23 -0500

    Update RankedGradeBook.cs
    bug fix

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -45,14 +45,14 @@ namespace GradeBook.GradeBooks
                 base.CalculateStatistics();
         }
 
-        public override void CalculateStudentStatistics(string name)
+        public override void CalculateStudentStatistics(string nombre)
         {
             if (Students.Count < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
             }
             else
-                base.CalculateStudentStatistics();
+                base.CalculateStudentStatistics(nombre);
         }
     }
 }
